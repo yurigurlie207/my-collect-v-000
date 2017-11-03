0cdb58661ae708fd361f82798e9731aae135d13e
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-03 16:36:33 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(myList)
+  collection = []
+  i=0
+  while i < myList.length
+    collection << yield(myList[i])
+    i +=1
+  end
+  collection
+end
 
-
+# my_collect(myList) { |name| name.split(" ").first }
