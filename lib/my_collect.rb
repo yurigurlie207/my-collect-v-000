bd90cf39bfa28a240b1afab23312cc2a524425ce
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-03 16:34:18 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,11 @@
 def my_collect(myList)
   collection = []
   i=0
-  while i <  
+  while i < myList.lenght
+    yield(myList[i])
+    i +=1
+  end
+  collectoin
 end
 
 my_collect(myList) { |name| name.split(" ").first }
